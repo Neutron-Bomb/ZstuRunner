4d8d4fbb3df4d2049aa7814477f8fe052fbc9695
Haren <human01d@icloud.com>
2022-06-13 20:04:11 +0800

    Initial Commit

---
+++ ZstuRunner/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ZstuRunner
+//
+//  Created by 陈驰坤 on 2022/6/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZstuRunner/ZstuRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZstuRunnerApp.swift
+//  ZstuRunner
+//
+//  Created by 陈驰坤 on 2022/6/13.
+//
+
+import SwiftUI
+
+@main
+struct ZstuRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
